944b6f726a6170ef27b259c868bbae73404c120e
albert203 <albert.jordaan@outlook.com>
2022-07-16 02:31:21 +1200

    Adding server script to NetworkManager
    This was done in the server project, I have set the port to 7777 and max client count to 10. You can adjust port according to the list in the "Multiplayer files" folder where a txt contains a link to unassigned ones. When you start the game a message appears in the console saying you have started a server using the specified port.

--- Server Project/Assets/Scripts/Multiplayer/NetworkManager.cs
+++ Server Project/Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
+using RiptideNetworking;
+using RiptideNetworking.Utils;
 using UnityEngine;
 
 public class NetworkManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // Attach the NetworkManager to a gameobject 
+    // and access that specific instance anywhere in our code 
+    // This will also ensure that there will ever be one instance of
+    // the NetworkManager in the scene.
+    private static NetworkManager _singleton; 
+
+    public static NetworkManager Singleton{
+        get => _singleton;
+        private set
+        {
+            if (_singleton == null)
+            {
+                _singleton = value;
+            }
+            else if(_singleton != value)
+            {
+                Debug.Log($"{nameof(NetworkManager)} instance already exists, destroying duplicates");
+                Destroy(value);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // We are going to need a server property 
+    public Server Server { get; private set; }
+
+    // We are going to need a field for the port and max client count
+    // ushort can declare var storing a short integer. between 0 and 65,535. 
+    [SerializeField] private ushort port;
+    [SerializeField] private ushort maxClientCount;
+
+    private void Awake()
     {
-        
+
+    }
+
+    // In the start method, we are going to intialise the Riplogger class
+    // This will allow us to see the Riptide log messages in the unity console.
+    // Then we are going to assign a value to our server property, and call the 
+    // Start method to start the server.
+    private void Start()
+    {   
+        // Initialise the Riptide logger class.
+        RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
+
+        // Starting the server. With our port and max client count parameters.
+        Server = new Server();
+        Server.Start(port, maxClientCount);
+    }
+
+    private void FixedUpdate()
+    {
+        // Ensures messages recieved from the server are processed.
+        Server.Tick();
+    }
+
+    private void OnApplicationQuit()
+    {
+        // When the application quits, we are going to call the Stop method
+        // on our server property.
+        Server.Stop();
     }
 }
